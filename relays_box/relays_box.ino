37e2be3be0a3d099ca92080065264b672f908984
charleau <djcharleau.ricard@gmail.com>
2022-12-01 23:03:34 -0500

    lcd 20x4 i2c added

--- relays_box/relays_box.ino
+++ relays_box/relays_box.ino
@@ -1,3 +1,6 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
 const int relay1 = 2;
 const int relay2 = 3;
 const int relay3 = 4;
@@ -7,10 +10,11 @@ const int relay6 = 7;
 const int relay7 = 8;
 const int relay8 = 9;
 
-int speedDelay = 2000;           //speed is in milliseconds
-int prgNb = 0;
+int speedDelay = 200;           //speed is in milliseconds
+int prgNb = 2;
 
-byte relaysPower = 0;
+byte relaysPower = 1;
+int sens = -2;
 
 unsigned long currentTime = 0;
 unsigned long previousTime = 0;
@@ -27,12 +31,36 @@ void setup() {
   pinMode(relay7, OUTPUT);
   pinMode(relay8, OUTPUT);
 
+  digitalWrite(relay1, HIGH);
+  digitalWrite(relay2, HIGH);
+  digitalWrite(relay3, HIGH);
+  digitalWrite(relay4, HIGH);
+  digitalWrite(relay5, HIGH);
+  digitalWrite(relay6, HIGH);
+  digitalWrite(relay7, HIGH);
+  digitalWrite(relay8, HIGH);
+
+  lcd.init();
+  lcd.backlight();
+
+  /*lcd.setCursor(0,0);
+  lcd.print("allo test");
+
+  delay(4000);*/
+
 }
 
 void loop() {
   timer();
   relayTask();
-  //Serial.println(0b10110);
+  
+  lcd.setCursor(0,0);
+  lcd.print("Programme : #");
+  lcd.print(prgNb);
+  lcd.setCursor(0,1);
+  lcd.print("  Vitesse : ");
+  lcd.print(speedDelay / 1000);
+  lcd.print("s");
 }
 
 void timer(){
@@ -40,53 +68,148 @@ void timer(){
 
   if(currentTime - previousTime >= speedDelay){
     previousTime = currentTime;
-    relaysPower = relaysPower * 2;
+    progTask();
   }
 }
 
 void progTask(){
   switch(prgNb){
-    case 0:
-
+    case 1:
+      if(relaysPower >= 8){
+      relaysPower = 1;      
+      }
+      else{
+        relaysPower++;
+      }      
+    break;
+    case 2:
+      if(relaysPower <= 1){
+      relaysPower = 8;      
+      }
+      else{
+        relaysPower--;
+      }      
+    break;
+    case 3:
+      if(relaysPower == 11){
+        relaysPower = 10;
+      }
+      else{
+        relaysPower = 11;
+       } 
     break;
   }
 }
 
 void relayTask(){
   
-      for(int i = 0; i < 10; i++){
+     /* for(int i = 0; i < 10; i++){
         digitalWrite(i, HIGH);
       }
-      
+      */
   switch(relaysPower){
-    case 0b0:                                   // ALL OFF
-      for(int i = 0; i < 10; i++){
-        digitalWrite(i, HIGH);
-      }
+    case 1:
+      digitalWrite(relay1, LOW);
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
+      digitalWrite(relay5, HIGH);
+      digitalWrite(relay6, HIGH);
+      digitalWrite(relay7, HIGH);
+      digitalWrite(relay8, HIGH);      
     break;
-    case 0b1:
-      digitalWrite(relay1, LOW);      
-    break;
-    case 0b10:
+    case 2:
+      digitalWrite(relay1, HIGH);
       digitalWrite(relay2, LOW);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
+      digitalWrite(relay5, HIGH);
+      digitalWrite(relay6, HIGH);
+      digitalWrite(relay7, HIGH);
+      digitalWrite(relay8, HIGH);
     break;
-    case 0b100:
+    case 3:
+      digitalWrite(relay1, HIGH);
+      digitalWrite(relay2, HIGH);
       digitalWrite(relay3, LOW);
+      digitalWrite(relay4, HIGH);
+      digitalWrite(relay5, HIGH);
+      digitalWrite(relay6, HIGH);
+      digitalWrite(relay7, HIGH);
+      digitalWrite(relay8, HIGH);
     break;
-    case 0b1000:
+    case 4:
+      digitalWrite(relay1, HIGH);
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
       digitalWrite(relay4, LOW);
+      digitalWrite(relay5, HIGH);
+      digitalWrite(relay6, HIGH);
+      digitalWrite(relay7, HIGH);
+      digitalWrite(relay8, HIGH);
     break;
-    case 0b10000:
+    case 5:
+      digitalWrite(relay1, HIGH);
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
       digitalWrite(relay5, LOW);
+      digitalWrite(relay6, HIGH);
+      digitalWrite(relay7, HIGH);
+      digitalWrite(relay8, HIGH);
     break;
-    case 0b100000:
+    case 6:
+      digitalWrite(relay1, HIGH);
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
+      digitalWrite(relay5, HIGH);
       digitalWrite(relay6, LOW);
+      digitalWrite(relay7, HIGH);
+      digitalWrite(relay8, HIGH);
     break;
-    case 0b1000000:
+    case 7:
+      digitalWrite(relay1, HIGH);
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
+      digitalWrite(relay5, HIGH);
+      digitalWrite(relay6, HIGH);
       digitalWrite(relay7, LOW);
+      digitalWrite(relay8, HIGH);
     break;
-    case 0b10000000:
+    case 8:
+      digitalWrite(relay1, HIGH);
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
+      digitalWrite(relay5, HIGH);
+      digitalWrite(relay6, HIGH);
+      digitalWrite(relay7, HIGH);
       digitalWrite(relay8, LOW);
     break;
+
+
+
+    case 10:
+      digitalWrite(relay1, HIGH);
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
+      digitalWrite(relay5, LOW);
+      digitalWrite(relay6, LOW);
+      digitalWrite(relay7, LOW);
+      digitalWrite(relay8, LOW);      
+    break;
+    case 11 :
+      digitalWrite(relay1, LOW);
+      digitalWrite(relay2, LOW);
+      digitalWrite(relay3, LOW);
+      digitalWrite(relay4, LOW);
+      digitalWrite(relay5, HIGH);
+      digitalWrite(relay6, HIGH);
+      digitalWrite(relay7, HIGH);
+      digitalWrite(relay8, HIGH);      
+    break;  
   }
 }
